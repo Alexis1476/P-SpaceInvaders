1be7615062dbe2f7bbcc887aadeb8e9c2ea5ac7a
AlexisR <halexisr20@hotmail.com>
2021-09-24 08:31:33 +0200

    Copie code d'origine

---
+++ MenuItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_SpaceInvaders
+{
+    class MenuItem
+    {
+        private int _itemId;
+        private string _text;
+        private Action _action;
+
+        /// <summary>
+        /// Constructor par défaut
+        /// </summary>
+        public MenuItem()
+        {
+
+        }
+        public MenuItem(int itemId, string text, Action action)
+        {
+            _itemId = itemId;
+            _text = text;
+            _action = action;
+        }
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+        public Action Action
+        {
+            get { return _action; }
+            set { _action = value; }
+        }
+    }
+}
---
+++ OptionSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_SpaceInvaders
+{
+    class OptionSwitch
+    {
+        private int _itemId;
+        private string _text;
+        private Action _action;
+    }
+}
