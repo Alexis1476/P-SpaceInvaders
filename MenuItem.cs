6b5c1bca2f30709a2a4b4bfcbd4f514087dd5f76
Alexis1476 <halexisr20@hotmail.com>
2021-10-15 18:57:13 +0200

    Ajout des membres class MenuItem

--- MenuItem.cs
+++ MenuItem.cs
@@ -8,6 +8,38 @@ namespace P_SpaceInvaders
 {
     class MenuItem
     {
-       
+        #region Attributs
+        private int _idItem;
+        private string _name;
+        private Action _action;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Constructor par texte et action de l'option
+        /// </summary>
+        /// <param name="name">Nom de l'option</param>
+        /// <param name="action">Function de l'option</param>
+        public MenuItem(string name, Action action)
+        {
+            _name = name;
+            _action = action;
+        }
+        /// <summary>
+        /// Constructor par Id, nom et action de l'option
+        /// </summary>
+        /// <param name="idItem">Identifiant de l'option</param>
+        /// <param name="name">Nom de l'option</param>
+        /// <param name="action">Function de l'option</param>
+        public MenuItem(int idItem, string name, Action action)
+        {
+            _idItem = idItem;
+            _name = name;
+            _action = action;
+        }
+        #endregion
+
+        #region Getteurs et setteurs
+        #endregion
     }
 }
