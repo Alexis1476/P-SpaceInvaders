b9e2970ffd639db4f143881f302300709553a2f1
Alexis1476 <halexisr20@hotmail.com>
2021-10-22 18:10:49 +0200

    Création membres class OptionSwitch et constructor dynamique (Config Son ou Difficulté)

--- OptionSwitch.cs
+++ OptionSwitch.cs
@@ -9,17 +9,52 @@ namespace P_SpaceInvaders
     class OptionSwitch
     {
         #region Attributs
+        /// <summary>
+        /// Id du paramètre
+        /// </summary>
         private int _idItem;
-        private string _text;     
+        /// <summary>
+        /// Nom du paramètre Sound ou Difficulty
+        /// </summary>
+        private string _name;
+        /// <summary>
+        /// Position sur l'axe X du nom du paramètre dans la console
+        /// </summary>
+        private int _posX;
+        /// <summary>
+        /// Position sur l'axe Y du nom du paramètre dans la console
+        /// </summary>
+        private int _posY;
+        /// <summary>
+        /// Liste string des differents options
+        /// </summary>
+        private List<string> _options;
         #endregion
 
         #region Constructors
+        public OptionSwitch(int idItem, string name)
+        {
+            //Si c'est pour parametrer le son
+            if (name=="Sound")
+            {
+                _idItem = idItem;
+                _name = name;
+                _options = new List<string> { "ON", "OFF" };
+            }
+            //Si c'est pour modifier la difficulté
+            else if (name=="Difficulty")
+            {
+                _idItem = idItem;
+                _name = name;
+                _options = new List<string> { "EASY", "NORMAL", "HARD" };
+            }
+        }
         #endregion
 
         #region Methodes
-        //public bool ChangeStatusOption()
+        //public bool ActivateSound()
         //{
-        //    if ()
+        //    if (3==2)
         //    {
         //        return true;
         //    }
@@ -28,6 +63,28 @@ namespace P_SpaceInvaders
         //        return false;
         //    }
         //}
+        public int ChangeDifficulty()
+        {
+            return 0;
+        }
+        #endregion
+
+        #region Getteurs et setteurs
+        public int PosX
+        {
+            get { return _posX; }
+            set { _posX = value; }
+        }
+        public int PosY
+        {
+            get { return _posY; }
+            set { _posY = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
         #endregion
     }
 }
