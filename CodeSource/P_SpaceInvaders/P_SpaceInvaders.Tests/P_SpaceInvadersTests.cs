cea6c41a333f2c3447145e9bff1b9330b871c799
Alexis1476 <halexisr20@hotmail.com>
2021-12-11 23:19:42 +0100

    Création dossiers + nouveau projet

---
+++ CodeSource/P_SpaceInvaders/P_SpaceInvaders.Tests/P_SpaceInvadersTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace P_SpaceInvaders.Tests
+{
+    [TestClass]
+    public class P_SpaceInvadersTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ CodeSource/P_SpaceInvaders/P_SpaceInvaders.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("P_SpaceInvaders.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("P_SpaceInvaders.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8f6d07e-de46-49a8-aadb-16c5c6022a04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
