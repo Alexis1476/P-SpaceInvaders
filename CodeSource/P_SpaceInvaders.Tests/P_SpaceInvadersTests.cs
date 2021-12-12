6ea3c1c652e88d022f47f9c592aec81b268efd95
Alexis1476 <halexisr20@hotmail.com>
2021-12-12 14:44:51 +0100

    4 Tests pour méthode IsPlaying

--- CodeSource/P_SpaceInvaders.Tests/P_SpaceInvadersTests.cs
+++ CodeSource/P_SpaceInvaders.Tests/P_SpaceInvadersTests.cs
@@ -7,9 +7,54 @@ namespace P_SpaceInvaders.Tests
     public class P_SpaceInvadersTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IsPlaying_With_Ship_Null_Result_False()
         {
+            // Arrange
             Game game = new Game(200, 200, 20);
+            game.Ship = null;
+
+            // Act
+            bool result = game.IsPlaying();
+
+            // Assert
+            Assert.AreEqual(false, result, "Le résultat doit être false");
+        }
+        [TestMethod]
+        public void IsPlaying_With_Ship_Not_Null_Result_True()
+        {
+            // Arrange
+            Game game = new Game(200, 200, 20);
+
+            // Act
+            bool result = game.IsPlaying();
+
+            // Assert
+            Assert.AreEqual(true, result, "Le résultat doit être true");
+        }
+        [TestMethod]
+        public void IsPlaying_With_0_Invaders_Result_False()
+        {
+            // Arrange
+            Game game = new Game(200, 200, 20);
+            game.Invaders.Clear();
+
+            // Act
+            bool result = game.IsPlaying();
+
+            // Assert
+            Assert.AreEqual(false, result, "Le résultat doit être false");
+        }
+        [TestMethod]
+        public void IsPlaying_With_Invaders_Not_Null_Result_True()
+        {
+            // Arrange
+            Game game = new Game(200, 200, 20);
+
+            // Act
+            bool result = game.IsPlaying();
+
+            // Assert
+            Assert.AreEqual(true, result, "Le résultat doit être true");
         }
     }
 }
