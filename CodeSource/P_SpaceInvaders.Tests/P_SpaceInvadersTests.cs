6d0e31de09cc88e7fab21caea585ccbd1a124b74
Alexis1476 <halexisr20@hotmail.com>
2021-12-13 16:32:58 +0100

    15 Tests unitaires

--- CodeSource/P_SpaceInvaders.Tests/P_SpaceInvadersTests.cs
+++ CodeSource/P_SpaceInvaders.Tests/P_SpaceInvadersTests.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿///ETML
+///Auteur : Alexis Rojas
+///Date : 26.11.2021
+///Description: Tests unitaires du projet P_SpaceInvaders
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using P_SpaceInvaders.GameObjects;
 
 namespace P_SpaceInvaders.Tests
 {
@@ -109,6 +113,106 @@ namespace P_SpaceInvaders.Tests
 
             // Assert
             Assert.AreEqual(false, result, "Le résultat doit être false");
+        }
+        [TestMethod]
+        public void CalculateDimensionsObject_With_Value_12345_Result_5()
+        {
+            // Arrange
+            Game game = new Game(50, 50, 5);
+            Ship ship = new Ship(game, "12345", 3);
+
+            // Act
+            int result = ship.WidthChars;
+
+            // Assert
+            Assert.AreEqual(5, result, "Le résultat doit être 5");
+        }
+        [TestMethod]
+        public void CalculateDimensionsObject_With_2DObject_Result_3()
+        {
+            // Arrange
+            Game game = new Game(50, 50, 5);
+            Ship ship = new Ship(game, "█\n█\n█", 3);
+
+            // Act
+            int result = ship.HeightChars;
+
+            // Assert
+            Assert.AreEqual(3, result, "Le résultat doit être 3");
+        }
+        [TestMethod]
+        public void Move_Ship_Left_With_ValuePosX_10_Result_9()
+        {
+            // Arrange
+            Game game = new Game(50, 50, 5);
+            game.Ship.PosX = 10;
+
+            // Act
+            game.Ship.Move(Direction.Left);
+            int result = game.Ship.PosX;
+
+            // Assert
+            Assert.AreEqual(9, result, "Le résultat doit être 9");
+        }
+        [TestMethod]
+        public void Move_Invader_Down_With_ValuePosY_10_Result_12()
+        {
+            // Arrange
+            Game game = new Game(50, 50, 5);
+            Invader invader = new Invader(2, game, "█\n█");
+            invader.PosY = 10;
+
+            // Act
+            invader.Move(Direction.Down);
+            int result = invader.PosY;
+
+            // Assert
+            Assert.AreEqual(10 + invader.HeightChars, result, "Le résultat doit être 12");
+        }
+        [TestMethod]
+        public void Invader_Fire_With_ValuePosX_0_And_WidthChars_3_Result_1()
+        {
+            // Arrange
+            Game game = new Game(50, 50, 5);
+            Invader invader = new Invader(2, game, "█x█");
+            invader.PosX = 0;
+
+            // Act
+            invader.Fire();
+            int result = game.Bullets[0].PosX;
+
+            // Assert
+            Assert.AreEqual(1, result, "Le résultat doit être 1");
+        }
+        [TestMethod]
+        public void Invader_Fire_With_ValuePosY_1_Result_2()
+        {
+            // Arrange
+            Game game = new Game(50, 50, 5);
+            Invader invader = new Invader(2, game, "█x█");
+            invader.PosY = 1;
+
+            // Act
+            invader.Fire();
+            int result = game.Bullets[0].PosY;
+
+            // Assert
+            Assert.AreEqual(invader.PosY + 1, result, "Le résultat doit être 1");
+        }
+        [TestMethod]
+        public void Ship_Fire_With_ValuePosY_10_Result_9()
+        {
+            // Arrange
+            Game game = new Game(50, 50, 5);
+            Ship ship = new Ship(game, "█x█", 3);
+            ship.PosY = 10;
+
+            // Act
+            ship.Fire();
+            int result = game.Bullets[0].PosY;
+
+            // Assert
+            Assert.AreEqual(ship.PosY - 1, result, "Le résultat doit être 9");
         }
     }
 }
