09f46ae2182a749a1e892056d1e001bdf4897e8f
Alexis1476 <halexisr20@hotmail.com>
2021-12-14 19:38:35 +0100

    Commentaires pour les tests unitaires

--- CodeSource/P_SpaceInvaders.Tests/P_SpaceInvadersTests.cs
+++ CodeSource/P_SpaceInvaders.Tests/P_SpaceInvadersTests.cs
@@ -5,11 +5,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using P_SpaceInvaders.GameObjects;
 
+/// <summary>
+/// Tests unitaires du projet P_SpaceInvaders
+/// </summary>
 namespace P_SpaceInvaders.Tests
 {
+    /// <summary>
+    /// Class qui contient les tests unitaires du projet
+    /// </summary>
     [TestClass]
     public class P_SpaceInvadersTests
     {
+        /// <summary>
+        /// Test methode Game.IsPlaying() lorsque le vaisseau est mort
+        /// </summary>
         [TestMethod]
         public void IsPlaying_With_Ship_Null_Result_False()
         {
@@ -23,6 +32,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(false, result, "Le résultat doit être false");
         }
+        /// <summary>
+        /// Test méthode Game.IsPlaying() lorsque le vaisseau est en vie
+        /// </summary>
         [TestMethod]
         public void IsPlaying_With_Ship_Not_Null_Result_True()
         {
@@ -35,6 +47,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(true, result, "Le résultat doit être true");
         }
+        /// <summary>
+        /// Test méthode Game.IsPlaying() lorsqu'il n'y a plus d'invaders
+        /// </summary>
         [TestMethod]
         public void IsPlaying_With_0_Invaders_Result_False()
         {
@@ -48,6 +63,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(false, result, "Le résultat doit être false");
         }
+        /// <summary>
+        /// Test méthode Game.IsPlaying() lorsque il y a des invaders
+        /// </summary>
         [TestMethod]
         public void IsPlaying_With_Invaders_Not_Null_Result_True()
         {
@@ -60,6 +78,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(true, result, "Le résultat doit être true");
         }
+        /// <summary>
+        /// Test méthode GameObject.IsInMap() lorsque le vaisseau sort de la map
+        /// </summary>
         [TestMethod]
         public void IsInMap_With_ShipPosY_Value_70_Result_false()
         {
@@ -73,6 +94,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(false, result, "Le résultat doit être false");
         }
+        /// <summary>
+        /// Test méthode GameObject.IsInMap() lorsque le vaisseau se trouve à l'interieur de la map
+        /// </summary>
         [TestMethod]
         public void IsInMap_With_ShipPosY_Value_40_Result_true()
         {
@@ -86,6 +110,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(true, result, "Le résultat doit être true");
         }
+        /// <summary>
+        /// Test méthode GameObject.IsAtCoordinates(int,int) lorsque le vaisseau se trouve à une position donnée
+        /// </summary>
         [TestMethod]
         public void IsAtCoordinates_WithValues_30_And_40_Result_True()
         {
@@ -100,6 +127,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(true, result, "Le résultat doit être true");
         }
+        /// <summary>
+        /// Test méthode GameObject.IsAtCoordinates(int,int) lorsque le vaisseau ne se trouve pas à une position donnée
+        /// </summary>
         [TestMethod]
         public void IsAtCoordinates_WithValues_30_And_50_Result_false()
         {
@@ -114,6 +144,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(false, result, "Le résultat doit être false");
         }
+        /// <summary>
+        /// Test méthode GameObject.CalculateDimensions() lorsque l'objet a 5 caractères de largeur
+        /// </summary>
         [TestMethod]
         public void CalculateDimensionsObject_With_Value_12345_Result_5()
         {
@@ -127,6 +160,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(5, result, "Le résultat doit être 5");
         }
+        /// <summary>
+        /// Test méthode GameObject.CalculateDimensions() lorsque l'objet a 1 caractère de hauteur
+        /// </summary>
         [TestMethod]
         public void CalculateDimensionsObject_With_2DObject_Result_3()
         {
@@ -140,6 +176,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(3, result, "Le résultat doit être 3");
         }
+        /// <summary>
+        /// Test méthode Ship.Move(Direction) vers la gauchee
+        /// </summary>
         [TestMethod]
         public void Move_Ship_Left_With_ValuePosX_10_Result_9()
         {
@@ -154,6 +193,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(9, result, "Le résultat doit être 9");
         }
+        /// <summary>
+        /// Test méthode Invader.Move(Direction) vers le bas
+        /// </summary>
         [TestMethod]
         public void Move_Invader_Down_With_ValuePosY_10_Result_12()
         {
@@ -169,6 +211,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(10 + invader.HeightChars, result, "Le résultat doit être 12");
         }
+        /// <summary>
+        /// Test méthode Invader.Fire() test de la position initiale de la balle en X
+        /// </summary>
         [TestMethod]
         public void Invader_Fire_With_ValuePosX_0_And_WidthChars_3_Result_1()
         {
@@ -184,6 +229,9 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(1, result, "Le résultat doit être 1");
         }
+        /// <summary>
+        /// Test méthode Invader.Fire() test de la position initiale de la balle en Y
+        /// </summary>
         [TestMethod]
         public void Invader_Fire_With_ValuePosY_1_Result_2()
         {
@@ -199,6 +247,27 @@ namespace P_SpaceInvaders.Tests
             // Assert
             Assert.AreEqual(invader.PosY + 1, result, "Le résultat doit être 1");
         }
+        /// <summary>
+        /// Test méthode Ship.Fire() test de la position initiale de la balle en X
+        /// </summary>
+        [TestMethod]
+        public void Ship_Fire_With_ValuePosX_0_And_WidthChars_3_Result_1()
+        {
+            // Arrange
+            Game game = new Game(50, 50, 5);
+            Ship ship = new Ship(game, "█x█", 3);
+            ship.PosX = 0;
+
+            // Act
+            ship.Fire();
+            int result = game.Bullets[0].PosX;
+
+            // Assert
+            Assert.AreEqual(1, result, "Le résultat doit être 1");
+        }
+        /// <summary>
+        /// Test méthode Ship.Fire() test de la position initiale de la balle en Y
+        /// </summary>
         [TestMethod]
         public void Ship_Fire_With_ValuePosY_10_Result_9()
         {
